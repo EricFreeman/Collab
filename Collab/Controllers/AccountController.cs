2a756a669ac6d99f1d036808fe4f32d4d597164f
Eric Freeman <eric.freeman@extendhealth.com>
2013-12-30 15:15:55 -0700

    minor refactoring and added an account controller and login page to eventually hook up

---
+++ Collab/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Collab.Models.Account;
+
+namespace Collab.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View(new LoginModel());
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            model.IsSuccessful = false;
+            return View(model);
+        }
+    }
+}
---
+++ Collab/Models/Account/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Collab.Models.Account
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public bool? IsSuccessful { get; set; }
+    }
+}
